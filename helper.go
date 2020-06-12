4275264442f9531b03f116e82b15d52b27aad75e
管宜尧 <mylxsw@aicode.cc>
2020-06-12 12:08:53 +0800

    add copy properties function

--- helper.go
+++ helper.go
@@ -1,9 +1,58 @@
 package coll
 
+import (
+	"errors"
+	"reflect"
+	"unsafe"
+)
+
 func Map(origin interface{}, dest interface{}, mapper interface{}) error {
 	return MustNew(origin).Map(mapper).All(dest)
 }
 
 func Filter(origin interface{}, dest interface{}, filter interface{}) error {
 	return MustNew(origin).Filter(filter).All(dest)
+}
+
+var ErrTargetIsNil = errors.New("target is nil")
+var ErrTargetInvalid = errors.New("target must be a pointer to struct")
+
+// CopyProperties copy exported properties(with same name and type) from source to target
+// target must be a pointer to struct
+func CopyProperties(source interface{}, target interface{}) error {
+	sourceRefVal := reflect.Indirect(reflect.ValueOf(source))
+	targetRefVal := reflect.ValueOf(target)
+
+	// 如果 source 为 null，则不需要拷贝任何属性
+	if !sourceRefVal.IsValid() {
+		return nil
+	}
+
+	if !targetRefVal.IsValid() {
+		return ErrTargetIsNil
+	}
+
+	if targetRefVal.Kind() != reflect.Ptr {
+		return ErrTargetInvalid
+	}
+
+	targetVal := targetRefVal.Elem()
+	targetType := targetVal.Type()
+
+	for i := 0; i < targetType.NumField(); i++ {
+		field := targetType.Field(i)
+		fieldName := field.Name
+		if fieldName[0] < 'A' || fieldName[0] > 'Z' {
+			continue
+		}
+
+		dst := sourceRefVal.FieldByName(fieldName)
+		if !dst.IsValid() || field.Type != dst.Type() {
+			continue
+		}
+
+		reflect.NewAt(field.Type, unsafe.Pointer(targetVal.Field(i).UnsafeAddr())).Elem().Set(dst)
+	}
+
+	return nil
 }
--- helper_test.go
+++ helper_test.go
@@ -2,8 +2,10 @@ package coll_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/mylxsw/coll"
+	"github.com/stretchr/testify/assert"
 )
 
 type Struct1 struct {
@@ -57,4 +59,95 @@ func TestFilter(t *testing.T) {
 	if dest[0].Name != "name2" {
 		t.Error("test failed")
 	}
+}
+
+
+type UserDomain struct {
+	ID           int64
+	Name         string
+	Email        string
+	Password     string
+	Gender       string
+	Roles        []Role
+	CreatedAt    time.Time
+	privateField string
+}
+
+type Role struct {
+	ID   int64
+	Name string
+}
+
+type UserView struct {
+	ID            int64
+	Name          string
+	Email         string
+	Gender        bool
+	Roles         []Role
+	HasPermission bool
+	privateField  string
+}
+
+func TestCopyProperties(t *testing.T) {
+
+	{
+		userDomain := UserDomain{
+			ID:       1,
+			Name:     "Tom",
+			Email:    "tom@aicode.cc",
+			Password: "2356565656",
+			Gender:   "MALE",
+			Roles: []Role{
+				{ID: 2, Name: "admin"},
+				{ID: 3, Name: "viewer"},
+			},
+			CreatedAt:    time.Now(),
+			privateField: "145",
+		}
+
+		var targetUserView UserView
+		assert.NoError(t, coll.CopyProperties(userDomain, &targetUserView))
+		assert.Equal(t, userDomain.ID, targetUserView.ID)
+		assert.Equal(t, userDomain.Name, targetUserView.Name)
+		assert.Equal(t, userDomain.Email, targetUserView.Email)
+		assert.Equal(t, false, targetUserView.Gender)
+		assert.Equal(t, false, targetUserView.HasPermission)
+		assert.Equal(t, userDomain.Roles, targetUserView.Roles)
+		assert.Equal(t, "", targetUserView.privateField)
+	}
+
+	{
+		userDomain := &UserDomain{
+			ID:        1,
+			Name:      "Tom",
+			Email:     "tom@aicode.cc",
+			Password:  "2356565656",
+			Gender:    "MALE",
+			CreatedAt: time.Now(),
+		}
+
+		var targetUserView UserView
+		assert.NoError(t, coll.CopyProperties(userDomain, &targetUserView))
+		assert.Equal(t, userDomain.ID, targetUserView.ID)
+		assert.Equal(t, userDomain.Name, targetUserView.Name)
+		assert.Equal(t, userDomain.Email, targetUserView.Email)
+		assert.Equal(t, false, targetUserView.Gender)
+		assert.Equal(t, false, targetUserView.HasPermission)
+	}
+
+	{
+		userDomain := UserDomain{
+			ID:        1,
+			Name:      "Tom",
+			Email:     "tom@aicode.cc",
+			Password:  "2356565656",
+			Gender:    "MALE",
+			CreatedAt: time.Now(),
+		}
+
+		var targetUserView UserView
+		assert.Equal(t, coll.ErrTargetInvalid, coll.CopyProperties(userDomain, targetUserView))
+		assert.Equal(t, coll.ErrTargetIsNil, coll.CopyProperties(userDomain, nil))
+	}
+
 }
